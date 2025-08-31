5b10f0f3de0ead3f340e8bd9e8cbb608d2fe8ea4
Tellinq <59412384+Tellinq@users.noreply.github.com>
2025-08-31 18:11:41 -0400

    Allow specifying the mouse type

--- src/main/java/com/cheatbreaker/nethandler/server/CBPacketSwingInhibitorUpdate.java
+++ src/main/java/com/cheatbreaker/nethandler/server/CBPacketSwingInhibitorUpdate.java
@@ -16,12 +16,22 @@ import java.io.IOException;
 @NoArgsConstructor
 public class CBPacketSwingInhibitorUpdate extends CBPacket {
 
+    private int type = 0;
+
     private int frequencyMs;
     private double decayRate;
     private int cpsCap;
 
+    public CBPacketSwingInhibitorUpdate(int frequencyMs, double decayRate, int cpsCap) {
+        this.type = 0;
+        this.frequencyMs = frequencyMs;
+        this.decayRate = decayRate;
+        this.cpsCap = cpsCap;
+    }
+
     @Override
     public void write(ByteBufWrapper out) throws IOException {
+        out.buf().writeInt(this.type);
         out.buf().writeInt(this.frequencyMs);
         out.buf().writeDouble(this.decayRate);
         out.buf().writeInt(this.cpsCap);
@@ -29,6 +39,7 @@ public class CBPacketSwingInhibitorUpdate extends CBPacket {
 
     @Override
     public void read(ByteBufWrapper in) throws IOException {
+        this.type = in.buf().readInt();
         this.frequencyMs = in.buf().readInt();
         this.decayRate = in.buf().readDouble();
         this.cpsCap = in.buf().readInt();
